4a21503e9d680b439e5bea3052d2bf952e51f7ec
kunzmainali <kunzmainali@hotmail.com>
2015-03-10 22:34:55 -0500

    swift

---
+++ Fizzbuzz.playground/section-1.swift
@@ -0,0 +1,18 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+for i in 1...20 {
+    if (i % 3 == 0) && (i % 5 == 0) {
+        println("Fizzbuzz")
+    }
+    else if (i % 3 == 0) {
+        println("Fizz")
+    }
+    else if (i % 5 == 0) {
+        println("Buzz")
+    }
+    else {
+        println(i)
+    }
+}
